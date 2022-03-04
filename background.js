da4deae5bb7f008d2999ef78cdfb063f3c896d34
Benedikt Böhm <bb@xnull.de>
2022-03-04 20:58:44 +0100

    run on card and other urls as well

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (
         changeInfo.status === "complete" &&
-        tab.url.indexOf("trello.com/b/") > -1
+        tab.url.indexOf("trello.com/") > -1
     ) {
         console.log("execute collape.js")
         chrome.scripting.executeScript({
