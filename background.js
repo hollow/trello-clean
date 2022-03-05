015e2d9c89369144de90e93b6cd462a2c37fc52d
Benedikt Böhm <bb@xnull.de>
2022-03-05 10:55:54 +0100

    use MutationObserver to remove badges from card front

--- background.js
+++ background.js
@@ -3,7 +3,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         changeInfo.status === "complete" &&
         tab.url.indexOf("trello.com/") > -1
     ) {
-        console.log("execute collape.js")
         chrome.scripting.executeScript({
             target: { tabId: tabId },
             files: ['collapse.js']
